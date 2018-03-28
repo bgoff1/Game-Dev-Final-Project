84549ee5e065568bd48cb4535ea29c5770a95649
bgoff1 <35714383+bgoff1@users.noreply.github.com>
2018-03-28 11:19:45 -0500

    Refactored some code
    - Removed some extra scenes that wont be used.
    - Renamed scenes to fit with sceme of game
    - Removed references to unused buttons in main menu script.

---
+++ The Dungeon/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public Button startButton;
+	public Button adventureButton;
+	public Button endlessButton;
+	public GameObject mainMenuPanel;
+	public GameObject difficultyPanel;
+    
+	void Start () {
+		mainMenuPanel.SetActive(true);
+		startButton.onClick.AddListener(showDifficulties);
+        adventureButton.onClick.AddListener(startAdventure);
+		endlessButton.onClick.AddListener(startEndless);
+		difficultyPanel.SetActive(false);
+	}
+
+	private void showDifficulties()
+	{
+		mainMenuPanel.SetActive(false);
+		difficultyPanel.SetActive(true);
+	}
+
+	#region  startFunctions
+	private void startAdventure()
+	{
+		SceneManager.LoadScene("AdventureMode");
+	}
+
+	private void startEndless()
+	{
+		SceneManager.LoadScene("EndlessMode");
+	}
+	#endregion
+}
