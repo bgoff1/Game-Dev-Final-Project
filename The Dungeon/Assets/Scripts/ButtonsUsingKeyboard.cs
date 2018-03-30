1a825238685e0d2665e490af49787b1a8c06dff0
bgoff1 <35714383+bgoff1@users.noreply.github.com>
2018-03-29 20:44:43 -0500

    Started implementing a keyboard variant for battle.
    - Keyboard needs work.
    - Battle might still need work not sure ??

---
+++ The Dungeon/Assets/Scripts/ButtonsUsingKeyboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsUsingKeyboard : MonoBehaviour {
+
+	public Image cursor;
+
+	private Button topLeft;
+	private Button topRight;
+	private Button botLeft;
+	private Button botRight;
+
+	void Start() 
+	{
+		// set up cursor to be at topleft and move based on input.
+			// move by setting sprite of one to nothing and setting sprite
+			// where to go to the cursor
+		topLeft = transform.Find("TopLeft").gameObject.GetComponent<Button>();
+		topRight = transform.Find("TopRight").gameObject.GetComponent<Button>();
+		botLeft = transform.Find("BotLeft").gameObject.GetComponent<Button>();
+		botRight = transform.Find("BotRight").gameObject.GetComponent<Button>();
+	}
+}
